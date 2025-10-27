fb7bca13925902e24d234ad6bfb3e692742cfbc0
Luis Neubauer <luis@sovt.de>
2025-10-27 15:14:48 +0100

    some flow testing plots mainly

--- zeugs/measurements.py
+++ zeugs/measurements.py
@@ -85,6 +85,13 @@ def radial_multiplicities(ns : int) -> list[int]:
 
 
 
+def radial_separations(ns : int) -> list[float]:
+    '''list of all reachable distances r on the lattice.'''
+
+    return [r2**.5 for r2 in radial_multiplicities(ns) if r2 != 0]
+
+
+
 
 def make_distance_corr_arrays(radial_corrs : list, normalize : bool) -> tuple[list, list]:
     '''Takes a list where the index corresponds to s^2.
@@ -189,6 +196,7 @@ if __name__ == '__main__':
         file = lambda ln: Path('outputs') / 'flow_err_test' / f'flow_ln={ln}.txt'
 
 
+
         # compute flows for various stepsizes
         for ln in range(max_log_steps + 1):
             if file(ln).exists(): continue
@@ -204,55 +212,74 @@ if __name__ == '__main__':
             file(ln).write_text(out)
 
 
+
         # extract final flowtime measurement for each stepsize ln
         finalmeas_for_ln = [parse_flow_output(file(ln).read_text())[-1] for ln in range(max_log_steps + 1)]
-        number_t_r2 = len(finalmeas_for_ln[0].q_corrs) * len(finalmeas_for_ln[0].q_corrs[0])
 
-        assert number_t_r2 == (nt//2) * (3*(ns//2)**2 + 1)
+        assert len(finalmeas_for_ln[0].q_corrs) == (nt//2)
+        assert len(finalmeas_for_ln[0].q_corrs[0]) == (3*(ns//2)**2 + 1)
         for ln in range(max_log_steps+1):
             assert finalmeas_for_ln[ln].flow_time == radius_to_flowtime(rf_max, nt)
+
 
         
         # extract G(s) data series for each ln and each tau
         corrsdist_ln_tau = []
         for meas in finalmeas_for_ln:
-            corrsdist_ln_tau.append([ make_distance_corr_arrays(corrs_r2, False)[1]
+            corrsdist_ln_tau.append([ make_distance_corr_arrays(corrs_r2, False)[1]     # calling make_dist... repeatedly could be made much faster
                                       for corrs_r2 in meas.q_corrs ])
 
 
-        # compute average difference of correlators between two neighbooring stepsizes
-        number_t_r = len(corrsdist_ln_tau[0]) * len(corrsdist_ln_tau[0][0])
-        print(len(corrsdist_ln_tau[0]), len(corrsdist_ln_tau[0][0]))
-        diff_for_ln    = []    # ln's elements is diff between ln and ln+1
+
+        # ln's elements is diff between ln and ln+1
+        diff_for_ln    = []    
         differr_for_ln = []   
 
+
+        # compute average difference of correlators C(t,r) between two different stepsizes
         for ln in range(max_log_steps):
-            diff = 0
+            sq_diff_vals = []
+
             for tslice0, tslice1 in zip(corrsdist_ln_tau[ln], corrsdist_ln_tau[ln+1]):
                 for corr0, corr1 in zip(tslice0, tslice1):
-                    diff += abs(corr0 - corr1)
-            diff_for_ln.append(diff / number_t_r)
+                    mid = (corr0 + corr1) / 2
+                    sq_diff_vals.append( (corr0 - corr1)**2 )
 
-            differr = 0
-            for tslice0, tslice1 in zip(corrsdist_ln_tau[ln], corrsdist_ln_tau[ln+1]):
-                for corr0, corr1 in zip(tslice0, tslice1):
-                    differr += (abs(corr0 - corr1) - diff_for_ln[ln])**2
-            differr_for_ln.append(np.sqrt(differr / (number_t_r-1)))
+            mean_sq_diff      = sum(sq_diff_vals[:100]) / len(sq_diff_vals[:100])
+            root_mean_sq_diff = np.sqrt(mean_sq_diff)
+            diff_for_ln.append(root_mean_sq_diff)
+
+            # do variance...
+            ...
 
 
-        # plot the stuff
-        plt.errorbar(
-            x      = [2**ln for ln in range(max_log_steps)],
-            y      = diff_for_ln,
-            yerr   = differr_for_ln,
-            marker = 'o',
-        )
-        # plt.ylim(0, diff_for_ln[1] * 1.5)
-        plt.yscale('log')
-        plt.xlabel('Number n of flow-steps')
-        plt.ylabel('Average absolute error between n and 2n flow-steps')
-        plt.savefig('plot_flow_wilson_error.png')
 
+        # plot the errors
+        if 1:
+            plt.errorbar(
+                x      = [2**ln for ln in range(max_log_steps)],
+                y      = diff_for_ln,
+                # yerr   = differr_for_ln,
+                marker = 'o',
+            )
+            # plt.ylim(0, diff_for_ln[1] * 1.5)
+            plt.yscale('log')
+            plt.xlabel('Number n of flow-steps')
+            plt.ylabel('Average absolute error between n and 2n flow-steps')
+            plt.savefig('plots/plot_flow_wilson_error.png')
+
+
+        # plot some correlators
+        if 0:
+            tau = 3
+            ln0, ln1 = max_log_steps-1, max_log_steps
+            r_vals = radial_separations(ns)[:100]
+            plt.scatter(r_vals, corrsdist_ln_tau[ln0][tau][:100], s=0.3, label=f'{tau=}, ln={ln0}')
+            plt.scatter(r_vals, corrsdist_ln_tau[ln1][tau][:100], s=0.3, label=f'{tau=}, ln={ln1}')
+            plt.xlabel('radial separation r')
+            plt.ylabel('C(tau,r)')
+            plt.legend()
+            plt.savefig('plots/plot_some_corrs_stepsizes.png')
 
 
 
