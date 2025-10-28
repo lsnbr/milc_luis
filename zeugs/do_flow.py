d8552bb7e68c4369c441b0551007820105ddbbe1
Luis Neubauer <luis@sovt.de>
2025-10-28 17:10:38 +0100

    begin rewriting and expanding flowerror tests

--- zeugs/do_flow.py
+++ zeugs/do_flow.py
@@ -1,47 +1,43 @@
+import os
+from pathlib import Path
+
+from flowing import flow_rkmk3, gen_input_initial
 
 
 
 
 
 
+ns = 64
+nt = 16
 
+stoptimes_stepsizes = [
+    (1, 0.2),
+]
 
-# do flow with prescibed stoptime and stepsize
-if 0:
+flow_type = 'zeuthen'
 
-    input_initial = gen_input_initial(ns, nt)
-    # lat_initial = Path('thermalized_configs') / f'ns{ns}_nt{nt}_T1p3_1000hb.lat'
-    
-    out = flow_rkmk3(
-        stoptime      = 0.5,
-        stepsize      = 0.5,
-        lat_initial   = Path('gauge_configs') / f'pg_00009.lat',
-        input_initial = input_initial,
-        flow          = 'wilson'
-    )
+lattice_i = 100
+lattice_initial = Path('/work/scratch/ln29bamu') / 'gauge_configs' / f'pg_{lattice_i:08}.lat'
 
-    (Path('outputs') / 'flow_test.txt').write_text(out)
+ncores = int(os.environ['SLURM_NTASKS'])
 
 
 
-# flow for ensemble
-if 0:
 
-    input_initial = gen_input_initial(ns, nt)
 
-    stoptime, stepsize = flow_params(10, nt, r_max=0.15)
 
-    for i_config in range(100):
-        print( '#################################################')
-        print(f'###############  config {i_config:05}  ##################')
-        print( '#################################################')
+# only works for one segment as of now
+out = flow_rkmk3(
+    stoptime      = stoptimes_stepsizes[0][0],
+    stepsize      = stoptimes_stepsizes[0][1],
+    lat_initial   = lattice_initial,
+    input_initial = gen_input_initial(ns, nt),
+    flow          = flow_type,
+    ncores        = ncores,
+    run_cmd       = 'srun'
+)
 
-        out = flow_rkmk3(
-            stoptime      = stoptime,
-            stepsize      = stepsize,
-            lat_initial   = Path('gauge_configs') / f'pg_{i_config:05}.lat',
-            input_initial = input_initial
-        )
 
-        (Path('outputs') / f'flow_out_{i_config:05}.txt').write_text(out)
+
 
